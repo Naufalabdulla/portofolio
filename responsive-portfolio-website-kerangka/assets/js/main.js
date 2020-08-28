2d910b694ef904e6bf2a09ec47f477ea3d43c0f7
sparkAOI <68763549+sparkAOI@users.noreply.github.com>
2020-08-28 17:07:44 +0700

    Add files via upload

---
+++ responsive-portfolio-website-kerangka/assets/js/main.js
@@ -0,0 +1,77 @@
+//===== MENU SHOW =====/ 
+const showMenu = (toogleId, navId) =>{
+    const toogle = document.getElementById(toogleId),
+    nav = document.getElementById(navId)
+
+    if(toogle && nav){
+        toogle.addEventListener('click', ()=>{
+            nav.classList.toogle('show')
+        })
+    }
+}
+showMenu('nav-toogle','nav-menu')
+//===== REMOVE MENU MOBILE =====/
+const navLink = document.querySelectorAll('nav__link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+//===== SCROLL SECTIONS ACTIVE LINK =====/
+const sections = documeny.querySelectorAll('section[id]')
+
+window.addEventListener('scroll', scrollActive)
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetheight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+
+        if(scollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active')
+        }else{
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active')
+        }
+    
+   })
+}
+//===== SCROLL REVEAL ANIMATION =====/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '80px',
+    duration: 2000,
+    reset: true
+})
+/*SCROLL HOME*/
+sr.reveal ('.home__title',{})
+sr.reveal ('.home__scroll',{delay:400})
+sr.reveal ('.home__img',{origin:'right',delay:400})
+
+/*SCROLL ABOUT*/
+sr.reveal ('.about__img',{delay:500})
+sr.reveal ('.about__subtitle',{delay:300})
+sr.reveal ('.about__profession',{delay:400})
+sr.reveal ('.about__text',{delay:500})
+sr.reveal (':about__social-icon',{delay:600,interval:200})
+
+/*SCROLL SKILLS*/
+sr.reveal ('.skills__subtitle',{})
+sr.reveal ('.skills__name',{distance:'28px',delay:50,interval:100})
+sr.reveal ('.skills__img',{delay:400})
+
+/*SCROLL PORTFOLIO*/
+sr.reveal ('.portofolio__img',{interval:200})
+
+/*SCROLL CONTACT*/
+sr.reveal ('.contact__subtitle',{})
+sr.reveal ('.contact__text',{nterval:200})
+sr.reveal ('.contact__input',{delay:400})
+sr.reveal ('.contact__button',{delay:500})
+
+
+
